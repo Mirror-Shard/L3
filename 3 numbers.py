a46a331754bb430755f8d5b46312bf2ed11cd72d
Mirror-Shard <Inncvation@outlook.com>
2021-03-24 00:31:41 +0300

    fix in numbers.py

--- 3 numbers.py
+++ 3 numbers.py
@@ -4,10 +4,8 @@ number3 = float(input("Third number: "))
 number4 = float(input("Fourth number: "))
 
 amount1 = number1+number2
-#float(amount1)
 amount2 = number3+number4
-#float(amount2)
 
 print("First number + second number = {0}" .format(amount1))
 print("Third number + fourth number = {0}" .format(amount2))
-print("Third number + fourth number = %.2f" % (amount1 / amount2))
+print("First amount + second amount = %.2f" % (amount1 / amount2))
